52a00a1bfd6e684bc24629ae91cfec69cc1667c9
Mike <lanselot777hailander@gmail.com>
2020-01-17 16:58:46 -0400

    Some fixes

--- trydjango/products/views.py
+++ trydjango/products/views.py
@@ -6,20 +6,20 @@ from .models import Product
 # Create your views here.
 
 #Pure Django Form
-def product_create_view(request):	
-	my_form = RawProductForm()
-	if request.method == "POST":
-		my_form = RawProductForm(request.POST)
-		if my_form.is_valid():
-			#now the data is good
-			print(my_form.cleaned_data)
-			Product.objects.create(**my_form.cleaned_data)
-		else:
-			print(my_form.errors)
-	context = {
-		"form": my_form
-	}
-	return render(request, "products/product_create.html", context)
+#def product_create_view(request):
+#	my_form = RawProductForm()
+#	if request.method == "POST":
+#		my_form = RawProductForm(request.POST)
+#		if my_form.is_valid():
+#			#now the data is good
+#			print(my_form.cleaned_data)
+#			Product.objects.create(**my_form.cleaned_data)
+#		else:
+#			print(my_form.errors)
+#	context = {
+#		"form": my_form
+#	}
+#	return render(request, "products/product_create.html", context)
 
 #Form with Raw Form
 #def product_create_view(request):
@@ -33,16 +33,16 @@ def product_create_view(request):
 #	return render(request, "products/product_create.html", context)
 
 #Form with ProductForm
-#def product_create_view(request):
-#	form = ProductForm(request.POST or None)
-#	if form.is_valid():
-#		form.save()
-#		form = ProductForm()
-#	
-#	context = {
-#		'form': form
-#	}
-#	return render(request, "products/product_create.html", context)
+def product_create_view(request):
+	form = ProductForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+		form = ProductForm()
+	
+	context = {
+		'form': form
+	}
+	return render(request, "products/product_create.html", context)
 
 
 def product_detail_view(request):
@@ -55,3 +55,13 @@ def product_detail_view(request):
 		'object': obj
 	}
 	return render(request, "products/product_detail.html", context)
+
+def render_initial_data(request):
+	initial_data = {
+		'title': "My this awesome title"
+	}
+	form = RawProductForm(request.POST or None, initial=initial_data)
+	context = {
+		'form': form
+	}
+	return render(request, "products/product_create.html", context)
