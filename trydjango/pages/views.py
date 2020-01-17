9ed0847421da49c9c29cf90951b2a184f3548455
Mike <lanselot777hailander@gmail.com>
2020-01-17 15:45:19 -0400

    Some changes

--- trydjango/pages/views.py
+++ trydjango/pages/views.py
@@ -2,5 +2,26 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>") # string of HTML code
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    #return HttpResponse("<h1>Hello World</h1>") # string of HTML code
+    return render(request, "home.html", {})
+
+def contact_view(request, *args, **kwargs):
+    #return HttpResponse("<h1>Contact Page</h1>")
+    return render(request, "contact.html", {})
+
+def about_view(request, *args, **kwargs):
+    #return HttpResponse("<h1>About Page</h1>")    
+    my_context = {
+    	"title": "abc this is about us",
+    	"my_text": "This is about us",
+    	"this_it_true": True,
+    	"my_number": 123,
+    	"my_list": [123, 424, 12313, "Abc"],
+    	"my_html": "<h1>Hello World</h1>"
+    }
+    return render(request, "about.html", my_context)
+
+def social_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Social Page</h1>")        
