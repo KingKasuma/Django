6870bfd3f57141daee997f40338f2fbeb4a42e5f
Mike <lanselot777hailander@gmail.com>
2020-01-17 22:55:14 -0400

    Some fixes

--- trydjango/players/views.py
+++ trydjango/players/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import numpy as np
+import pandas as pd
 
 # Create your views here.
 
@@ -8,8 +10,15 @@ def data_view(request):
 	#	'title': obj.title,
 	#	'description': obj.description
 	#}
+
+	df = pd.DataFrame(np.random.random((4,4)),
+                 index=['exp1','exp2','exp3','exp4'],
+                 columns=['Jan2015','Feb2015','Mar2015','Apr2015'])
+	print(df.to_json)				 
+
 	context = {
 		'object': {"id": 2},
-		'title': "some title"
+		'title': "some title",
+		'data': df.to_json
 	}
 	return render(request, "players/players.html", context)
